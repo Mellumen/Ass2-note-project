20ab2b4bf71961bc657a22cda39cded27b2cb807
Kristoffer <kriss_kb@hotmail.com>
2012-09-24 13:31:16 +0200

    test

--- Code/Ass2Note/src/com/example/ass2note/MainActivity.java
+++ Code/Ass2Note/src/com/example/ass2note/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    		//hei
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
