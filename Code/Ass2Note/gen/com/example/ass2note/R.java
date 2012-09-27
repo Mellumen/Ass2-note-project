d01f236c71ea6ed10965efcb614cde7b9ae9fa1c
SolveigS <SolveigHig@gmail.com>
2012-09-27 11:55:57 +0200

    omg good luck all.. i wish this git works..

--- Code/Ass2Note/gen/com/example/ass2note/R.java
+++ Code/Ass2Note/gen/com/example/ass2note/R.java
@@ -40,15 +40,15 @@ public final class R {
         public static final int title=0x7f090002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int list_row=0x7f030001;
-        public static final int list_view=0x7f030002;
-        public static final int note_edit=0x7f030003;
-        public static final int noteblock_activity=0x7f030004;
-        public static final int top_note=0x7f030005;
+        public static final int list_row=0x7f030000;
+        public static final int list_view=0x7f030001;
+        public static final int note_edit=0x7f030002;
+        public static final int noteblock_activity=0x7f030003;
+        public static final int top_note=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
+        public static final int activity_note=0x7f080001;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
@@ -60,6 +60,9 @@ public final class R {
         public static final int menu_insert=0x7f060006;
         public static final int menu_settings=0x7f060002;
         public static final int no_notes=0x7f060005;
+        public static final int position=0x7f06000d;
+        public static final int remindInfo=0x7f06000e;
+        public static final int time=0x7f06000c;
         public static final int title=0x7f060008;
         public static final int title_activity_main=0x7f060003;
         public static final int title_activity_note=0x7f060004;
