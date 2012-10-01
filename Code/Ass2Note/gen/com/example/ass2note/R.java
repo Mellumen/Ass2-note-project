a32b1c58e3869ad378e946a0698f9eab09d81b58
SolveigS <SolveigHig@gmail.com>
2012-10-01 10:00:07 +0200

    Added again some gps stuff

--- Code/Ass2Note/gen/com/example/ass2note/R.java
+++ Code/Ass2Note/gen/com/example/ass2note/R.java
@@ -25,34 +25,38 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int pin=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090001;
-        public static final int LinearLayout2=0x7f090009;
-        public static final int RelativeLayout1=0x7f09000a;
-        public static final int body=0x7f090005;
-        public static final int button1=0x7f09000d;
-        public static final int button_new_note=0x7f09000b;
-        public static final int confirm=0x7f090006;
-        public static final int date=0x7f090004;
-        public static final int dateButton=0x7f090007;
-        public static final int menu_settings=0x7f09000e;
-        public static final int text1=0x7f090000;
-        public static final int textView1=0x7f09000c;
-        public static final int time=0x7f090003;
-        public static final int timeButton=0x7f090008;
-        public static final int title=0x7f090002;
+        public static final int LinearLayout1=0x7f090002;
+        public static final int LinearLayout2=0x7f09000a;
+        public static final int RelativeLayout1=0x7f09000b;
+        public static final int body=0x7f090006;
+        public static final int button1=0x7f09000e;
+        public static final int button_new_note=0x7f09000c;
+        public static final int confirm=0x7f090007;
+        public static final int date=0x7f090005;
+        public static final int dateButton=0x7f090008;
+        public static final int mapView=0x7f090000;
+        public static final int menu_settings=0x7f09000f;
+        public static final int text1=0x7f090001;
+        public static final int textView1=0x7f09000d;
+        public static final int time=0x7f090004;
+        public static final int timeButton=0x7f090009;
+        public static final int title=0x7f090003;
     }
     public static final class layout {
-        public static final int list_row=0x7f030000;
-        public static final int list_view=0x7f030001;
-        public static final int note_edit=0x7f030002;
-        public static final int noteblock_activity=0x7f030003;
-        public static final int top_note=0x7f030004;
+        public static final int activity_google_maps=0x7f030000;
+        public static final int list_row=0x7f030001;
+        public static final int list_view=0x7f030002;
+        public static final int note_edit=0x7f030003;
+        public static final int noteblock_activity=0x7f030004;
+        public static final int top_note=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
-        public static final int activity_note=0x7f080001;
+        public static final int activity_google_maps=0x7f080000;
+        public static final int activity_main=0x7f080001;
+        public static final int activity_note=0x7f080002;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
